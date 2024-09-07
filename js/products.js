f4f724ae878b49f4b223db046d1e573e92853716
carqc <c.chales96@gmail.com>
2024-09-07 18:48:45 -0300

    Product de todas las categorías
    Toma el JSON según el ID de la categoría, mostrando todos los productos y no solo los autos

--- js/products.js
+++ js/products.js
@@ -1,4 +1,5 @@
-const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json"; // URL que contiene los datos que queremos mostrar
+let categoria = localStorage.getItem("catID");
+const DATA_URL = `https://japceibal.github.io/emercado-api/cats_products/${categoria}.json`; // URL que contiene los datos que queremos mostrar
 
 const contenedor  = document.getElementById("c_autos");
 
