8a1295e4dd6463ad745dff23c308acf10f4e5606
carqc <carolinachalesit@gmail.com>
2024-09-09 21:37:17 -0300

    Corrección link de JSON
    Corrección link de JSON y modificaciones acorde

--- js/product-info.js
+++ js/product-info.js
@@ -6,56 +6,58 @@ if (sessionStorage.getItem("sesion")) {
 /*FIN Nombre Usuario en Barra: ENTREGA 2*/
 
 /*INICIO leectura JSON*/
-let categoria = localStorage.getItem("catID");
-const DATA_URL = `https://japceibal.github.io/emercado-api/cats_products/${categoria}.json`; // URL que contiene los datos que queremos mostrar
+let ID_PROD = localStorage.getItem("prodID");
+const DATA_URL = `https://japceibal.github.io/emercado-api/products/${ID_PROD}.json`; // URL que contiene los datos que queremos mostrar
 
 
 const contenedorTodo  = document.getElementById("div_contenedorTodo"); /*YA LO AGREGUÉ EN HTML*/
 
 function showData (item) {
     let prod = document.createElement("div");
-    prod.className = "producto";                    /*CLASE QUE SE PUEDEN USAR EN HTML, SE PUEDE AGREGAR MÁS CON prod.classList.add ("nuevaClase"); - Para agregar ID usar prod.id = "nuevoID"; (usarlo solo en elementos únicos)*/
-        let imagen = document.createElement ("img");
-        imagen.className = "img";                       /*CLASE QUE SE PUEDEN USAR EN HTML, SE PUEDE AGREGAR MÁS*/
-        imagen.src = item.image;
-        prod.appendChild(imagen);
+    prod.className = "producto";                    /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS CON prod.classList.add ("nuevaClase"); - Para agregar ID usar prod.id = "nuevoID"; (usarlo solo en elementos únicos)*/
+        /*INICIO BLOQUE IMÁGEN*/
+        let imagenes = document.createElement ("div");
+        imagenes.className = "galeria";                       /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
+            for (let i of item.images){
+                let imagen = document.createElement ("img");
+                imagen.className = "img";                       /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
+                imagen.src = i;
+                imagenes.appendChild(imagen);
+            }
+        prod.appendChild(imagenes);
+        /*FIN BLOQUE IMÁGEN*/
+        /*INICIO BLOQUE TEXTO*/
         let textodiv = document.createElement("div");   /* div para todo texto, no tiene porqué ser así*/
-        textodiv.className = "img";                     /*CLASE QUE SE PUEDEN USAR EN HTML, SE PUEDE AGREGAR MÁS*/
+        textodiv.className = "img";                     /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
             let nombre = document.createElement("div");
-            nombre.className = "nombreproducto";        /*CLASE QUE SE PUEDEN USAR EN HTML, SE PUEDE AGREGAR MÁS*/
+            nombre.className = "nombreproducto";        /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
             nombre.appendChild(document.createTextNode(item.name));
                 textodiv.appendChild(nombre);
                 let descrip = document.createElement("div");
-                descrip.className = "descripcion";      /*CLASE QUE SE PUEDEN USAR EN HTML, SE PUEDE AGREGAR MÁS*/
+                descrip.className = "descripcion";      /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
                 descrip.appendChild(document.createTextNode(item.description));
                 textodiv.appendChild(descrip);          
                 let costo = document.createElement("div");
-                costo.className = "precio";             /*CLASE QUE SE PUEDEN USAR EN HTML, SE PUEDE AGREGAR MÁS*/
+                costo.className = "precio";             /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
                 costo.appendChild(document.createTextNode(item.currency + " " + item.cost));
                 textodiv.appendChild(costo);
                 let cantVend = document.createElement("div");
-                cantVend.className = "nota";            /*CLASE QUE SE PUEDEN USAR EN HTML, SE PUEDE AGREGAR MÁS*/
+                cantVend.className = "nota";            /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
                 cantVend.appendChild(document.createTextNode(item.soldCount + " cantidad de vendidos"));
                 textodiv.appendChild(cantVend);  
             prod.appendChild(textodiv);
+            /*INICIO BLOQUE TEXTO*/            
         contenedorTodo.appendChild(prod);
 }
 
-function prodSeleccionado (dataArray) {
-    let id = localStorage.getItem("prodID");
-    for (const item of dataArray) {
-        if (item.id == id){
-            return item;
-        }
-}
-}
+
 
 function respuesta (response) {
     return response.json();
   }
   
   function datos (data) {
-    return showData (prodSeleccionado (data.products));
+    return showData (data);
   }
   
   function esError(error){
