aea99147f34c9cbb0b3c50db12eddd86aec6788b
meugenia-c <eugecurbelo@gmail.com>
2024-09-12 18:08:12 -0300

    Actualización producto-info y css estilos

--- js/product-info.js
+++ js/product-info.js
@@ -1,78 +1,109 @@
 /*INICIO Nombre Usuario en Barra: ENTREGA 2*/
 if (sessionStorage.getItem("sesion")) {
-    let nombre = localStorage.getItem("usuario");
-    document.getElementById("nom_usuario").innerHTML = nombre;
+  let nombre = localStorage.getItem("usuario");
+  document.getElementById("nom_usuario").innerHTML = nombre;
 }
 /*FIN Nombre Usuario en Barra: ENTREGA 2*/
 
 /*INICIO leectura JSON*/
-let ID_PROD = localStorage.getItem("prodID");
-const DATA_URL = `https://japceibal.github.io/emercado-api/products/${ID_PROD}.json`; // URL que contiene los datos que queremos mostrar
+let categoria = localStorage.getItem("catID");
+const DATA_URL = `https://japceibal.github.io/emercado-api/cats_products/${categoria}.json`; // URL que contiene los datos que queremos mostrar
 
 
 const contenedorTodo  = document.getElementById("div_contenedorTodo"); /*YA LO AGREGUÉ EN HTML*/
 
 function showData (item) {
-    let prod = document.createElement("div");
-    prod.className = "producto";                    /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS CON prod.classList.add ("nuevaClase"); - Para agregar ID usar prod.id = "nuevoID"; (usarlo solo en elementos únicos)*/
-        /*INICIO BLOQUE IMÁGEN*/
-        let imagenes = document.createElement ("div");
-        imagenes.className = "galeria";                       /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
-            for (let i of item.images){
-                let imagen = document.createElement ("img");
-                imagen.className = "img";                       /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
-                imagen.src = i;
-                imagenes.appendChild(imagen);
-            }
-        prod.appendChild(imagenes);
-        /*FIN BLOQUE IMÁGEN*/
-        /*INICIO BLOQUE TEXTO*/
-        let textodiv = document.createElement("div");   /* div para todo texto, no tiene porqué ser así*/
-        textodiv.className = "img";                     /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
-            let nombre = document.createElement("div");
-            nombre.className = "nombreproducto";        /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
-            nombre.appendChild(document.createTextNode(item.name));
-                textodiv.appendChild(nombre);
-                let descrip = document.createElement("div");
-                descrip.className = "descripcion";      /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
-                descrip.appendChild(document.createTextNode(item.description));
-                textodiv.appendChild(descrip);
-                let cat = document.createElement("div");
-                cat.className = "cat";             /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
-                cat.appendChild(document.createTextNode(item.category));
-                textodiv.appendChild(cat);          
-                let costo = document.createElement("div");
-                costo.className = "precio";             /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
-                costo.appendChild(document.createTextNode(item.currency + " " + item.cost));
-                textodiv.appendChild(costo);
-                let cantVend = document.createElement("div");
-                cantVend.className = "nota";            /*CLASE QUE SE PUEDEN USAR EN CSS, SE PUEDE AGREGAR MÁS*/
-                cantVend.appendChild(document.createTextNode(item.soldCount + " cantidad de vendidos"));
-                textodiv.appendChild(cantVend);  
-            prod.appendChild(textodiv);
-            /*INICIO BLOQUE TEXTO*/            
-        contenedorTodo.appendChild(prod);
+  let prod = document.createElement("div");
+  prod.className = "productocatego";   /*CLASE QUE SE PUEDEN USAR EN HTML, SE PUEDE AGREGAR MÁS CON prod.classList.add ("nuevaClase"); - Para agregar ID usar prod.id = "nuevoID"; (usarlo solo en elementos únicos)*/
+      let textodiv = document.createElement("div"); /* div para todo texto */
+      textodiv.className = "estructuradiv";    
+
+              let nombre = document.createElement("div");
+              nombre.className = "nombreproductos";
+              textodiv.appendChild(nombre);
+              nombre.appendChild(document.createTextNode(item.name));
+
+              let titulocosto = document.createElement("div"); /* Texto Precio: -------------------- !!*/
+              titulocosto.className = "tituloproducto"
+              titulocosto.appendChild(document.createTextNode("Precio:"));
+              textodiv.appendChild(titulocosto); 
+
+              let costo = document.createElement("div");
+              costo.className = "precio";            
+              costo.appendChild(document.createTextNode(item.currency + " " + item.cost));
+              textodiv.appendChild(costo);
+
+              let titulodescrip = document.createElement("div"); /* Texto Descripción: -------------------- !!*/
+              titulodescrip.className = "tituloproducto"
+              titulodescrip.appendChild(document.createTextNode("Descripción:"));
+              textodiv.appendChild(titulodescrip); 
+              
+              let descrip = document.createElement("div");
+              descrip.className = "descripcion";     
+              descrip.appendChild(document.createTextNode(item.description));
+              textodiv.appendChild(descrip);
+
+              let titulocatego = document.createElement("div"); /* Texto Categoría: -------------------- !!*/
+              titulocatego.className = "tituloproducto"
+              titulocatego.appendChild(document.createTextNode("Categoría:"));
+              textodiv.appendChild(titulocatego); 
+              
+              let catego = document.createElement("div"); /* Categoría -------------------- !!*/
+              catego.className = "catego";
+              catego.appendChild(document.createTextNode(item.category));
+              textodiv.appendChild(catego);  
+              
+              let titulonota = document.createElement("div"); /* Texto Cantidad Vendidos: -------------------- !!*/
+              titulonota.className = "tituloproducto"
+              titulonota.appendChild(document.createTextNode("Cantidad de vendidos:"));
+              textodiv.appendChild(titulonota); 
+
+              let cantVend = document.createElement("div");
+              cantVend.className = "nota";            
+              cantVend.appendChild(document.createTextNode(item.soldCount));
+              textodiv.appendChild(cantVend); 
+
+          prod.appendChild(textodiv);
+
+          let tituloimagenes = document.createElement("div"); /* Imágenes ilustrativas: -------------------- !!*/
+              tituloimagenes.className = "tituloproducto"
+              tituloimagenes.appendChild(document.createTextNode("Imágenes ilustrativas:"));
+              textodiv.appendChild(tituloimagenes); 
+
+
+          let imagen = document.createElement ("img");
+          imagen.className = "imgprod";                      
+          imagen.src = item.image;
+          prod.appendChild(imagen);
+      contenedorTodo.appendChild(prod);
 }
 
-
+function prodSeleccionado (dataArray) {
+  let id = localStorage.getItem("prodID");
+  for (const item of dataArray) {
+      if (item.id == id){
+          return item;
+      }
+}
+}
 
 function respuesta (response) {
-    return response.json();
-  }
-  
-  function datos (data) {
-    return showData (data);
-  }
-  
-  function esError(error){
-    console.error ("Ocurrió error", error);
-  }
-  
-  function mostrarProducto () {
-    fetch(DATA_URL)
-    .then(respuesta)
-    .then(datos)
-    .catch(esError);
-  }
-  
-  mostrarProducto();
+  return response.json();
+}
+
+function datos (data) {
+  return showData (prodSeleccionado (data.products));
+}
+
+function esError(error){
+  console.error ("Ocurrió error", error);
+}
+
+function mostrarProducto () {
+  fetch(DATA_URL)
+  .then(respuesta)
+  .then(datos)
+  .catch(esError);
+}
+
+mostrarProducto();
