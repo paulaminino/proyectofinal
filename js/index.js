9bcf4e73885d4083a82198d1ab7d81c745c1b148
Paula Miniño. <pato_m@outlook.com>
2024-10-13 18:26:16 -0300

    Creamos el menu desplegable desde js

--- js/index.js
+++ js/index.js
@@ -13,10 +13,22 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
- 
-        if (!localStorage.getItem("usuario")){
-            dropdown.classList.add('oculto');
-        }
+
+/*Se crea el menú desplegable*/
+if (localStorage.getItem("usuario")){
+    let menuHTML = `
+    <div class="dropdown" >
+    <button class="nav-link btn dropdown-toggle autoClose" id="nom_usuario"  href="#" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    </button> <!-- Nombre Usuario en Barra: ENTREGA 2 / Transformamos el nombre de usuario en menú desplegable: ENTREGA 5-->
+    <ul class="dropdown-menu" id="dropdown2" >
+      <li><a class="dropdown-item" href="sell.html">Mi carrito</a></li>
+      <li><a class="dropdown-item" href="my-profile.html">Mi perfil</a></li>
+      <li><a class="dropdown-item" href="#" id="closesession">Cerrar Sesión</a></li>
+    </ul>
+  </div>
+`;
+dropdown.innerHTML = menuHTML;
+}
 
     if (!sessionStorage.getItem("sesion")){ 
         alert("Detectamos que no iniciaste sesión, redirigiendo para iniciar sesión");
@@ -29,7 +41,6 @@ document.addEventListener("DOMContentLoaded", function(){
         document.getElementById("nom_usuario").innerHTML = nombre;  
     }
     /*FIN Nombre Usuario en Barra: ENTREGA 2*/
-
 
 /*Borramos nombre de usuario de localStorage al cerrar sesión: ENTREGA 5*/
 document.getElementById("closesession").addEventListener('click', function() {
