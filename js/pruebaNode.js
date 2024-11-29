ff722e26f1930b03ae0136d6efa2948eafbb8006
Paula Miniño. <pato_m@outlook.com>
2024-11-29 18:47:56 -0300

    Actualizacion para verificar token con Loana

--- js/pruebaNode.js
+++ js/pruebaNode.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const mariadb = require('mariadb');
 const cors = require('cors');
+const authorize = require('authorize');
 
 const pool = mariadb.createPool({
   host: "localhost", 
@@ -13,9 +14,6 @@ const pool = mariadb.createPool({
 const app = express();
 const SECRET = 'CLAVE_SECRETTA';
 const port = 3000; 
-
-/*app.get('/ecommerce-data', middleware-autorizacion, (req, res)) =>*/
-/*const middleware-autorizacion = require('./middleware-autorizacion');*/
 
 
 app.use(express.json()); 
@@ -43,6 +41,25 @@ app.post("/login", async (req, res) =>{
   res.status(500).send('Internal server error'); } 
 
 });
+
+const authorize = (req, res, next) => { // req: solicitud, res: respuesta, next: funcion que pasa el control al siguiente middleware o ruta y si no se llama se frena el flujo de la solicitud.
+
+  const token = req.headers['authorization']; // se obtiene el token del header de la solicitud
+
+  if (!token) { // comprueba que la solicitud incluya un token
+      return res.status(401).json({ message: 'Acceso denegado.'});
+    }
+
+  try {
+      const decoded = jwt.verify(token.replace('Bearer ', ''), SECRET);  // verificar el token, elimina Bearer que es el formato en el que se obtiene el token (Bearer <token>) para utilizar solo el último
+      req.user = decoded; // añadir los datos del usuario decodificado a la solicitud
+      next(); // continuar 
+  } catch (err) {
+      return res.status(403).json({message: 'El token no es válido o ya expiró.'});
+  }
+}
+
+module.exports = authorize;
   
 
 // Iniciar servidor 
