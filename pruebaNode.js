8deb45ba7b5b7ef88fbcf24a3ed08d5e28c6687f
Loana Clark <loana200167@gmail.com>
2024-11-29 21:08:22 -0300

    Update pruebaNode.js
    actualizacion validacion de token

--- pruebaNode.js
+++ pruebaNode.js
@@ -2,7 +2,6 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const mariadb = require('mariadb');
 const cors = require('cors');
-let authorize = require('authorize');
 
 const pool = mariadb.createPool({
   host: "localhost", 
@@ -42,24 +41,25 @@ app.post("/login", async (req, res) =>{
 
 });
 
-authorize = (req, res, next) => { // req: solicitud, res: respuesta, next: funcion que pasa el control al siguiente middleware o ruta y si no se llama se frena el flujo de la solicitud.
+// verificacion del token
 
-  const token = req.headers['authorization']; // se obtiene el token del header de la solicitud
+app.use("/products", (req, res, next) =>{
 
-  if (!token) { // comprueba que la solicitud incluya un token
-      return res.status(401).json({ message: 'Acceso denegado.'});
+    const token = req.headers['authorization'];
+
+    if (!token) { // comprueba que la solicitud incluya un token
+        return res.status(401).json({ message: 'Acceso denegado.'});
+      }
+    
+    try {
+        const decoded = jwt.verify(token, SECRET);  // verificar el token
+        console.log(decoded);
+        req.user = decoded; // añadir los datos del usuario decodificado a la solicitud
+        next(); // continuar 
+    } catch (err) {
+        return res.status(403).json({message: 'El token no es válido o ya expiró.'});
     }
-
-  try {
-      const decoded = jwt.verify(token.replace('Bearer ', ''), SECRET);  // verificar el token, elimina Bearer que es el formato en el que se obtiene el token (Bearer <token>) para utilizar solo el último
-      req.user = decoded; // añadir los datos del usuario decodificado a la solicitud
-      next(); // continuar 
-  } catch (err) {
-      return res.status(403).json({message: 'El token no es válido o ya expiró.'});
-  }
-}
-
-module.exports = authorize;
+});
   
 
 // Iniciar servidor 
